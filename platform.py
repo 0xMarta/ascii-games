71843e914c3ddf667fe9ffef68d8c9875d7e1937
0xMarta <marta.z.bargiel@gmail.com>
2026-09-05 13:35:18 +0200

    add better gravity, block u can stand on and shooting

--- platform.py
+++ platform.py
@@ -1,10 +1,6 @@
 import curses
 import time
 import random
-import curses
-import random
-import time
-
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(True)
@@ -16,7 +12,19 @@ def main(stdscr):
     floor = my - 3
     frames = 0
     jump = 0
+    old_x = 0
+    old_y = 0
+    oldxx = 0
+    oldxy = 0
+    xx = 0
+    xy = 0
+    shot = False
     curses.init_pair(1,curses.COLOR_GREEN, curses.COLOR_BLACK)
+    mapa = [[0 for i in range(mx)] for j_ in range(my) ]
+    block1x = random.randint(3,mx - 3)
+    block1y = random.randint(my - 8,my - 3)
+    block2x = random.randint(3,mx - 3)
+    block2y = random.randint(my // 3,my - 3)
     while True:
         stdscr.erase()
         stdscr.border()
@@ -28,23 +36,49 @@ def main(stdscr):
         if key == ord(" ") and jump < 3:
             py -= 10
             jump += 1
+        if key == ord("x"):
+            xx = px + 1
+            xy = py
+            shot = True
         if key == ord("q"):
             return
-        for i in range(3,mx - 3):
-            try:
-                stdscr.addstr(floor, i, "-")
-            except curses.error :
-                pass
-        try:
-            stdscr.addstr(py, px, "&")
-        except curses.error:
-            pass
-        if py < floor - 1 and frames % 2 == 0 :
+        if mapa[py+1][px] == 0 and frames != 0:
             py += 1
             frames = 0
         if py == floor -1:
             jump = 0
+        mapa[block1y][block1x] = 3
+        mapa[block2y][block2x] = 3
+        mapa[floor] = [1 for n in range(mx)]
+        mapa[py][px] = 2
+        mapa[xy][xx] = 4
+
+        for y in range(2,my-2):
+            for x in range(2,mx-2):
+                try:
+                    if mapa[y][x] == 1:
+                        stdscr.addstr(y,x, "-")
+                    elif mapa[y][x] == 2:
+                        stdscr.addstr(y,x, "&")
+                    elif mapa[y][x] == 3:
+                        stdscr.addstr(y,x, "@")
+                    elif mapa[y][x] == 4:
+                        stdscr.addstr(y,x, ".")
+                except curses.error :
+                    pass
+        if shot:
+            xx += 1
+        if mapa[xy][min(xx + 1, mx - 1)] != 0 :
+            shot = False
+        mapa[oldxy][oldxx] = 0
+        mapa[old_y][old_x] = 0
+        if xx == mx - 1:
+            shot = False
         frames += 1
+        oldxx = xx
+        oldxy = xy
+        old_x = px
+        old_y = py
         stdscr.refresh()
         time.sleep(1/30)
 
