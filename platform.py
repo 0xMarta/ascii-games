e4e4b6ad038b40245f700cec96aae41b18cb78fe
0xMarta <marta.z.bargiel@gmail.com>
2026-09-05 17:54:20 +0200

    fix

--- platform.py
+++ platform.py
@@ -20,13 +20,18 @@ def main(stdscr):
     oldxy = 0
     oldenemyx = 0
     oldenemyy = 0
+    old_leftx = 0
+    old_lefty = 0
     xx = 0
     xy = 0
+    leftx = 0
+    lefty = 0
     shot = False
+    leftshot = False
     curses.init_pair(1,curses.COLOR_GREEN, curses.COLOR_BLACK)
     mapa = [[0 for i in range(mx)] for j_ in range(my) ]
     block1x = random.randint(3,mx - 3)
-    block1y = my -2  #random.randint(my - 8,my - 3)
+    block1y = my -3  #random.randint(my - 8,my - 3)
     block2x = random.randint(3,mx - 3)
     block2y = random.randint(my // 3,my - 3)
     while True:
@@ -44,6 +49,10 @@ def main(stdscr):
             xx = px + 1
             xy = py
             shot = True
+        if key == curses.KEY_LEFT:
+            leftx = px - 1
+            lefty = py
+            leftshot = True
         if key == ord("q"):
             return
         if mapa[py+1][px] == 0 and frames not in [0,1,2]:
@@ -58,6 +67,7 @@ def main(stdscr):
         mapa[floor] = [1 for n in range(mx)]
         mapa[py][px] = 2
         mapa[xy][xx] = 4
+        mapa[lefty][leftx] = 4
 
         for y in range(2,my-2):
             for x in range(2,mx-2):
@@ -76,17 +86,36 @@ def main(stdscr):
                     pass
         if shot:
             xx += 1
+        if leftshot:
+            leftx -=1
         if mapa[xy][min(xx + 1, mx - 1)] != 0 :
             shot = False
+        if mapa[lefty][max(leftx - 1, 3)] != 0 :
+            leftshot = False
         mapa[oldxy][oldxx] = 0
         mapa[old_y][old_x] = 0
+        mapa[old_lefty][old_leftx] = 0
         mapa[oldenemyy][oldenemyx] = 0
         if xx == mx - 1:
             shot = False
+        if leftx == mx - 1:
+            leftshot = False
+        if (xx, xy) == (enemyx, enemyy):
+            mapa[xy][xx] == 0
+            score += 1
+            xx = 0
+            yy = 0
+        if (leftx, lefty) == (enemyx, enemyy):
+            mapa[xy][xx] == 0
+            score += 1
+            leftx = 0
+            lefty = 0
         oldxx = xx
         oldxy = xy
         old_x = px
         old_y = py
+        old_leftx = leftx
+        old_lefty = lefty
         oldenemyy = enemyy
         oldenemyx = enemyx
         if px < enemyx and frames == 3 and mapa[enemyy][enemyx -1] == 0:
@@ -103,7 +132,10 @@ def main(stdscr):
         if frames == 4:
             frames = 0
         frames += 1
-        # enemyx = random.choice([min(mx, enemyx- random.choice([0,1])), max(3, enemyx + random.choice([0,1]))])
+        try:
+            stdscr.addstr(my - 5,5,f"score: {score}")
+        except curses.error:
+            pass
         stdscr.refresh()
         time.sleep(1/30)
 
